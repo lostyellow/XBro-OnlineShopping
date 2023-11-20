90ebd07fa86af5064251cb8994ee568f5a88a08f
JnCipA <144996206+JnCipA@users.noreply.github.com>
2023-11-20 10:48:22 +0800

    Add files via upload

---
+++ OnlineShopping/src/main/java/servlet/ProcessOrderStatusServlet.java
@@ -0,0 +1,59 @@
+package servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import dao.GoodsDao;
+import dao.UserDao;
+import dao.impl.GoodsDaoImpl;
+import dao.impl.UserDaoImpl;
+
+
+@WebServlet("/ProcessOrderStatusServlet")
+public class ProcessOrderStatusServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public ProcessOrderStatusServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+    
+    protected void success(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		int transaction_id = Integer.parseInt(request.getParameter("transaction_id"));
+		UserDao ud = new UserDaoImpl();
+		int product_id = ud.closedeal(transaction_id);
+		GoodsDao gd = new GoodsDaoImpl();
+		gd.takeOffGood(product_id);
+		request.getSession().removeAttribute("goods");
+		response.sendRedirect("detail.jsp");
+	}
+    
+    protected void fail(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    	int transaction_id = Integer.parseInt(request.getParameter("transaction_id"));
+    	UserDao ud = new UserDaoImpl();
+    	ud.listingGood(transaction_id);
+		response.sendRedirect("detail.jsp");
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String method = request.getParameter("method");
+		if("success".equals(method)) {
+			success(request, response);
+		}else if("fail".equals(method)) {
+			fail(request, response);
+		}
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		request.setCharacterEncoding("UTF-8");
+		response.setCharacterEncoding("UTF-8");
+		doGet(request, response);
+	}
+
+}
