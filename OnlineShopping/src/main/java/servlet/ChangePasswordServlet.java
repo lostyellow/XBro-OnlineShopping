2a031a612229be74248e6bc06a0d4b6736a90f63
JnCipA <144996206+JnCipA@users.noreply.github.com>
2023-11-21 21:47:15 +0800

    Update ChangePasswordServlet.java

--- OnlineShopping/src/main/java/servlet/ChangePasswordServlet.java
+++ OnlineShopping/src/main/java/servlet/ChangePasswordServlet.java
@@ -7,49 +7,58 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import dao.GoodsDao;
+import bean.DealList;
+import bean.User;
 import dao.UserDao;
-import dao.impl.GoodsDaoImpl;
 import dao.impl.UserDaoImpl;
 
-
-@WebServlet("/ProcessOrderStatusServlet")
-public class ProcessOrderStatusServlet extends HttpServlet {
+@WebServlet("/ChangePasswordServlet")
+public class ChangePasswordServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    public ProcessOrderStatusServlet() {
+
+	public ChangePasswordServlet() {
         super();
-        // TODO Auto-generated constructor stub
     }
-    
-    protected void success(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		int transaction_id = Integer.parseInt(request.getParameter("transaction_id"));
-		UserDao ud = new UserDaoImpl();
-		 ud.closedeal(transaction_id);
-		response.sendRedirect("record.jsp");
-	}
-    
-    protected void fail(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-    	int transaction_id = Integer.parseInt(request.getParameter("transaction_id"));
-    	UserDao ud = new UserDaoImpl();
-    	int product_id = ud.unfreezeGood(transaction_id);
-    	GoodsDao gd = new GoodsDaoImpl();
-		gd.takeOnGood(product_id);
-		response.sendRedirect("record.jsp");
+	
+	protected void verifyOldPwd(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+		User curUser = (User)request.getSession().getAttribute("curUser");
+		String oldPwd = request.getParameter("OldPwd");
+		String newPwd1 = request.getParameter("NewPassword1");
+		String newPwd2 = request.getParameter("NewPassword2");
+		String pbm = "";
+		if(oldPwd != "" && newPwd1 != "" && newPwd2 != "") {
+			if(oldPwd.equals(curUser.getPassword())) {//判断密码是不是正确
+				 if(oldPwd.equals(newPwd1)) {//密码正确判断新旧是不是一样的
+					 pbm = "samePwd";
+				 }else if(!newPwd1.equals(newPwd2)) {//判断新密码的两个是不是一样的
+					 pbm = "notSameNewPwd";
+				 }else { //都一样去更改密码
+					 UserDao ud = new UserDaoImpl();
+					 int userId = ud.findSeller_ID(curUser); 
+					 System.out.println("------------------"+userId);
+					 ud.changePwd(userId, newPwd1);
+					 request.getSession().removeAttribute("curUser");
+					 response.sendRedirect("login.jsp");	
+					 return;
+				 }
+			}else{//旧密码不一致
+				pbm = "oldPwdWrong";
+			}
+		}else {
+			pbm = "pwdnull";
+		}
+		request.setAttribute("pbm", pbm);
+		request.getRequestDispatcher("change_password.jsp").forward(request, response);
 	}
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		String method = request.getParameter("method");
-		if("success".equals(method)) {
-			success(request, response);
-		}else if("fail".equals(method)) {
-			fail(request, response);
+		if("changepwd".equals(method)) {
+			verifyOldPwd(request, response);
 		}
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		request.setCharacterEncoding("UTF-8");
 		response.setCharacterEncoding("UTF-8");
 		doGet(request, response);
