6f51cfa913c65fe5d7d8d637c002cb61516eb6c9
JnCipA <144996206+JnCipA@users.noreply.github.com>
2023-11-25 14:30:49 +0800

    add onlyoneuser

--- OnlineShopping/src/main/java/dao/UserDao.java
+++ OnlineShopping/src/main/java/dao/UserDao.java
@@ -5,6 +5,7 @@ import bean.*;
 public interface UserDao{
 /*对Users表的操作*/
 public void register(User user);//注册功能
+public String onlyOneUser();//判断是否只有一个商家
 public Users findAll();//遍历数据库查找账户密码
 public Boolean checkLogin(String username, String password);//username不能重复
 public int findSeller_ID(User user);//查找卖家ID
